7af34fa74c99e26ad5df74b7418a6e3fb0b26438
konmenel <konmenel@gmail.com>
2021-11-06 11:14:32 +0000

    print function works for all numbers now

--- joker_cal/joker_cal.cpp
+++ joker_cal/joker_cal.cpp
@@ -12,7 +12,7 @@ double power(double num, int pwr)
     return result;
 }
 
-void print_chech(int value)
+void print_check(int value)
 {
     if (value < 10)
         std::cout << "00" << value;
@@ -23,27 +23,36 @@ void print_chech(int value)
 }
 
 void print_number_with_commas(int num)
-{
+{   
+    // find where the first comma goes
+    int n = 3;
+    while ((num / power(10, n)) > 1)
+    {
+        n += 3;
+    }
+
+    n -= 3;
+
     int high, low, printed_value;
-    for (int i = 6; i > 0; i -= 3)
+    for (int i = n; i > 0; i -= 3)
     {
         high = (int)num / power(10, i + 3);
         low = (int)num / power(10, i);
 
         printed_value = low - high * 1000;
-        if (i != 6)
-            print_chech(printed_value);
+        if (i != n)
+            print_check(printed_value);
         else
             std::cout << printed_value;
         std::cout << ",";
     }
     printed_value = num - low * 1000;
-    print_chech(printed_value);
+    print_check(printed_value);
 }
 
 int main()
 {   
-    float TARGET = 0.1;
+    float TARGET = 0.5;
     double x = 45.0 * 44.0 * 43.0 * 42.0 * 41.0 * 20.0 / 120.0;
     x = 1 / x;
     x = 1 - x; // probability of losing
@@ -85,7 +94,8 @@ int main()
     
     std::cout << "You have " << (1 - TARGET) * 100 << "% chance of winning after ";
     print_number_with_commas(count);
-    std::cout << " joker draws." << std::endl;
-    std::cout << count << std::endl;
+    std::cout << " joker draws or ";
+    print_number_with_commas((int) count / 104);
+    std::cout<< " years" << std::endl;
 }
 
